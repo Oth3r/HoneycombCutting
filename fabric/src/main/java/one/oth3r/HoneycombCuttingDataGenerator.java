6af8f9d7b280e01507d9aecedce643cc510566d4
Oth3r <oth3rone@outlook.com>
2024-09-07 19:54:34 -0500

    1.20.4

--- fabric/src/main/java/one/oth3r/HoneycombCuttingDataGenerator.java
+++ fabric/src/main/java/one/oth3r/HoneycombCuttingDataGenerator.java
@@ -4,14 +4,11 @@ import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
-import net.minecraft.block.Blocks;
-import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.data.server.recipe.RecipeExporter;
 import net.minecraft.data.server.recipe.SingleItemRecipeJsonBuilder;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.book.RecipeCategory;
-
-import java.util.function.Consumer;
 
 public class HoneycombCuttingDataGenerator implements DataGeneratorEntrypoint {
 	@Override
@@ -27,7 +24,7 @@ public class HoneycombCuttingDataGenerator implements DataGeneratorEntrypoint {
 		}
 
 		@Override
-		public void generate(Consumer<RecipeJsonProvider> exporter) {
+		public void generate(RecipeExporter exporter) {
 			SingleItemRecipeJsonBuilder.createStonecutting(Ingredient.ofItems(Items.HONEYCOMB_BLOCK), RecipeCategory.MISC, Items.HONEYCOMB, 4)
 					.criterion(hasItem(Items.HONEYCOMB_BLOCK), conditionsFromItem(Items.HONEYCOMB_BLOCK)).offerTo(exporter, "honeycombcutting:honeycomb_cutting");
 
